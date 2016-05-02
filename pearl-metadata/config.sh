e7dc029966ed1880dc4d972049fc5c92116bef31
Filippo Squillace <squillace.filippo@gmail.com>
2016-05-02 13:44:47 +0100

    Add symlink for executable

--- pearl-metadata/config.sh
+++ pearl-metadata/config.sh
@@ -1,8 +1,3 @@
-if [[ $PATH != *"${PEARL_PKGDIR}/bin"* ]]
-then
-    PATH=$PATH:$PEARL_PKGDIR/bin
-fi
-
 if [[ $MANPATH != *"${PEARL_PKGDIR}/module/man"* ]]
 then
     MANPATH=$MANPATH:$PEARL_PKGDIR/module/man
--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -1,4 +1,10 @@
+function post_install(){
+    ln -s "${PEARL_PKGDIR}/bin/kyrat" ${PEARL_HOME}/bin
+}
+
 function pre_remove(){
+    rm -f ${PEARL_HOME}/bin/kyrat
+
     rm -f ${HOME}/.config/kyrat/bashrc.d/pearl_* 2> /dev/null
     rm -f ${HOME}/.config/kyrat/inputrc.d/pearl_* 2> /dev/null
     rm -f ${HOME}/.config/kyrat/vimrc.d/pearl_* 2> /dev/null

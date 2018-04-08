9269d9886aa2f1396b60e03d89bd136920467e91
Filippo Squillace <squillace.filippo@gmail.com>
2018-04-08 12:05:44 +1000

    :art: Refactor integ tests

--- tests/integ-tests/integ-tests-common.sh
+++ tests/integ-tests/integ-tests-common.sh
@@ -14,7 +14,4 @@ pearl install dot-bash
 kyrat -v -o "StrictHostKeyChecking no" localhost -- "[[ \\\$EDITOR == \"vim\" ]]"
 [ -e $HOME/.config/kyrat/bashrc.d/pearl_bash.sh ]
 
-pearl update test
-pearl remove test
-
 pearl remove sesaila dot-bash dot-vim

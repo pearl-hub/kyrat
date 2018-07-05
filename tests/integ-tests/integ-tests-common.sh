e054a643a744120e4bfdb542b061e60ca400574e
Filippo Squillace <squillace.filippo@gmail.com>
2018-07-05 21:19:48 +1000

    Add the dependencies via emerge

--- tests/integ-tests/integ-tests-common.sh
+++ tests/integ-tests/integ-tests-common.sh
@@ -3,20 +3,13 @@
 # This is executed on bash, zsh and fish.
 
 
-pearl install default/dot-vim
-pearl update test
-
-[ ! -e $HOME/.config/kyrat/bashrc.d/pearl_aliases.sh ]
-pearl install default/sesaila
-pearl update test
+[ -e $HOME/.config/kyrat/bashrc.d/pearl_aliases.sh ]
+[ -e $HOME/.config/kyrat/bashrc.d/pearl_bash.sh ]
+[ -e $HOME/.config/kyrat/inputrc.d/pearl_inputrc ]
+[ -e $HOME/.config/kyrat/vimrc.d/pearl_vimrc ]
 
 kyrat -v -o "StrictHostKeyChecking no" localhost -- q
-[ -e $HOME/.config/kyrat/bashrc.d/pearl_aliases.sh ]
 
-[ ! -e $HOME/.config/kyrat/bashrc.d/pearl_bash.sh ]
-pearl install default/dot-bash
-pearl update test
 kyrat -v -o "StrictHostKeyChecking no" localhost -- "[[ \\\$EDITOR == \"vim\" ]]"
-[ -e $HOME/.config/kyrat/bashrc.d/pearl_bash.sh ]
 
 pearl remove sesaila dot-bash dot-vim
